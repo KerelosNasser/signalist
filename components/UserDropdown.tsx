4612e7b16e3d6b04ecbc911286044e8ce8d63834
KerelosNasser <keronaser2030@gmail.com>
2026-03-17 04:11:30 -0700

    feat: Add UserDropdown component for user authentication and remove test-db.mjs.

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -41,7 +41,7 @@ const UserDropdown = ({
           className="flex items-center gap-3 text-gray-4 hover:text-yellow-500"
         >
           <Avatar className="h-8 w-8">
-            <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
+            <AvatarImage src="https://picsum.photos/id/237/536/354" />
             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
               {user.name[0]}
             </AvatarFallback>
